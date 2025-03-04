8289e316754557834a787837f5b55dd32cf00547
Artem <akkmine3@gmail.com>
2025-03-04 18:11:04 +0300

    Первая сдача

---
+++ task1.py
@@ -0,0 +1,3 @@
+rr = float(input("Введите радиус круга: "))
+S = float(3.14159 * (rr) ** 2)
+print("S = " + str(S))
---
+++ task2.py
@@ -0,0 +1,6 @@
+print("Дано уравнение вида ax + b = 0. Введите значения a и b для решения")
+a = float(input("A = "))
+b = float(input("B = "))
+x = float((-b) / a)
+
+print("X = " + str(x))
